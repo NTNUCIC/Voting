ad6968927223eb787465b0cbd54d51c18d4b572b
ryans610 <ryans610@gmail.com>
2017-06-01 16:00:10 +0800

    #5 add runtime php setting for error message display

--- autoload.php
+++ autoload.php
@@ -1,8 +1,15 @@
 <?php
+//php setting override
+define("SERVER_ENV","PROD");
+error_reporting(E_ALL);
+if(SERVER_ENV=="PROD") {
+    display_errors(false);
+    log_errors(true);
+}
+//autoload
 function autoload($className)
 {
     $className=ltrim($className, "\\");
-    //echo "Class:".$className."<br>";
     $fileName="class/";
     if($lastNsPos=strrpos($className, "\\")) {
         $namespace=substr($className, 0, $lastNsPos);
@@ -10,7 +17,6 @@ function autoload($className)
         $fileName.=str_replace("\\", DIRECTORY_SEPARATOR, $namespace).DIRECTORY_SEPARATOR;
     }
     $fileName.=$className.".php";
-    //echo "FilePath:".$fileName."<br>";
     require_once($fileName);
 }
 spl_autoload_register("autoload");
